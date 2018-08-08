ed4d03950beb522f2eee159a16b45c3bdb9a14bb
Carina Nanan <2021carinan@chapin.edu>
2018-08-08 01:16:32 +0000

    store

--- assets/js/teensClassics.js
+++ assets/js/teensClassics.js
@@ -1,17 +1,16 @@
 let books = [
-    { "title": "Adventures of Tom Sawyer", "author": "", "section": "teensClassics", "image": "https://prodimage.images-bn.com/pimages/9781593081393_p0_v1_s192x300.jpg"  },
-    { "title": "Treasure Island", "author": "", "section": "teensClassics", "image": "https://prodimage.images-bn.com/pimages/9781593082475_p0_v4_s192x300.jpg"  },
-    { "title": "Little Women", "author": "", "section": "teensClassics", "image": "https://prodimage.images-bn.com/pimages/9781593081089_p0_v4_s192x300.jpg"  },
     { "title": "", "author": "", "section": "teensClassics", "image": ""  },
     { "title": "", "author": "", "section": "teensClassics", "image": ""  },
-    { "title": "The Hunchback of Notre Dame", "author": "", "section": "teensClassics", "image": "https://prodimage.images-bn.com/pimages/9781593081409_p0_v1_s192x300.jpg"  },
-    { "title": "Man in the Iron Mask", "author": "", "section": "teensClassics", "image": "https://prodimage.images-bn.com/pimages/9781593082338_p0_v1_s192x300.jpg"  },
-    { "title": "The Woman in White", "author": "", "section": "teensClassics", "image": "https://prodimage.images-bn.com/pimages/9781593082802_p0_v4_s192x300.jpg"  },
-    { "title": "Ivanhoe", "author": "", "section": "teensClassics", "image": "https://prodimage.images-bn.com/pimages/9781593082468_p0_v1_s192x300.jpg"  },
-    { "title": "", "author": "", "section": "teensClassics", "image": "https://prodimage.images-bn.com/pimages/9781593082109_p0_v1_s192x300.jpg"  },
-    { "title": "The Jungle Book", "author": "", "section": "teensClassics", "image": "https://prodimage.images-bn.com/pimages/9781593081096_p0_v1_s192x300.jpg"  },
-    { "title": "Vanity Fair", "author": "", "section": "teensClassics", "image": "https://prodimage.images-bn.com/pimages/9781593080716_p0_v4_s192x300.jpg"  }
-
+    { "title": "", "author": "", "section": "teensClassics", "image": ""  },
+    { "title": "", "author": "", "section": "teensClassics", "image": ""  },
+    { "title": "", "author": "", "section": "teensClassics", "image": ""  },
+    { "title": "", "author": "", "section": "teensClassics", "image": ""  },
+    { "title": "", "author": "", "section": "teensClassics", "image": ""  },
+    { "title": "", "author": "", "section": "teensClassics", "image": ""  },
+    { "title": "", "author": "", "section": "teensClassics", "image": ""  },
+    { "title": "", "author": "", "section": "teensClassics", "image": ""  },
+    { "title": "", "author": "", "section": "teensClassics", "image": ""  },
+    { "title": "", "author": "", "section": "teensClassics", "image": ""  }
 ]
 
 //link image
