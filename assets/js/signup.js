a86a61d3b7e9aecfe42445ed5721c5fe405fa9dd
Carina Nanan <2021carinan@chapin.edu>
2018-08-08 10:27:07 +0000

    new everything

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,6 +1,7 @@
 let loginButton=document.querySelector("#button")
 let userDiv= document.querySelector("#user")
-let logoutButton = document.querySelector("#outButton")
+
+// let logoutButton = document.querySelector("#outButton")
 
 loginButton.addEventListener("click",e=>{
     console.log("button clicked")
@@ -12,29 +13,29 @@ function signInWithGoogle(){
     firebase.auth().signInWithPopup(googleAuthProvider)
     .then(function(data){
         console.log(data)
-        // addUserToScreen(data)
+        addUserToScreen(data)
     })
 }
 
-// function addUserToScreen(userData){
-//     let userName = userData.user.displayName
-//     localStorage.setItem("name",userName)
-//     userDiv.innerHTML = `Welcome, ${localStorage.getItem("name")}, `
-//     let userEmail = userData.user.email
-//     // localStorage.setItem("email", userEmail)
-//     // userDiv.innerHTML += `${localStorage.getItem("email")}.`
-//     // let userPicture = userData.user.photoURL 
-//     // userDiv.innerHTML +=`Photo: <img src ="${picture}">`
-// }
+function addUserToScreen(userData){
+    let userName = userData.user.displayName
+    localStorage.setItem("name",userName)
+    userDiv.innerHTML = `Welcome, ${localStorage.getItem("name")}, `
+    let userEmail = userData.user.email
+    localStorage.setItem("email", userEmail)
+    userDiv.innerHTML += `${localStorage.getItem("email")}.`
+    // let userPicture = userData.user.photoURL 
+    // userDiv.innerHTML +=`Photo: <img src ="${picture}">`
+}
 
-// // window.onLoad=startup
+window.onLoad=startup
 
 function startup(){
     if (localStorage.getItem("name")!= null){
           userDiv.innerHTML = `Welcome, ${localStorage.getItem("name")}, `
     }
   else {
-      userDiv.innerHTML= "Please log in by pressing the button."
+      loginButton.innerHTML= "Please log in by pressing the button."
   }
   
      if (localStorage.getItem("email")!= null){
